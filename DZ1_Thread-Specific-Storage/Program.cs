659dc1d84fc5089db377ccbf51b2045c59f52cf1
Eugegene <evgen151218@gmail.com>
2024-05-15 22:10:24 +0300

    thradss

---
+++ DZ1_Thread-Specific-Storage/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+
+namespace DZ1_Thread_Specific_Storage
+{
+    class Program
+    {
+
+        static ThreadLocal<int> threadLocalValue = new ThreadLocal<int>(() =>
+        {
+            // Викликатиметься раз на потік для ініціалізації значення
+            return Thread.CurrentThread.ManagedThreadId;
+        });
+
+        static void Main(string[] args)
+        {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            Console.InputEncoding = System.Text.Encoding.GetEncoding(1251);
+            Console.OutputEncoding = System.Text.Encoding.GetEncoding(1251);
+
+            Thread[] threads = new Thread[5];
+            for (int i = 0; i < threads.Length; i++)
+            {
+                threads[i] = new Thread(ThreadMethod);
+                threads[i].Start();
+            }
+
+            foreach (Thread t in  threads)
+            {
+                t.Join();
+            }
+            Console.ReadLine();
+        }
+
+        static void ThreadMethod()
+        {
+            Console.WriteLine($"Потік {Thread.CurrentThread.ManagedThreadId}: Значення локального потоку = {threadLocalValue.Value}");
+
+            threadLocalValue.Value = Thread.CurrentThread.ManagedThreadId * 100;
+
+            Console.WriteLine($"Потік {{Thread.CurrentThread.ManagedThreadId}}: Змінене значення локального потоку = {threadLocalValue.Value}");
+        }
+    }
+}
